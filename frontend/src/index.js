54e0045ab8fbb9843d41be74ed5e7d4963149671
Ashlesha-17 <ashleshamandhare17@gmail.com>
2026-02-05 17:52:30 +0530

    Remove basename and fix routing for Vercel

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const basename =
-  process.env.NODE_ENV === 'production'
-    ? '/event-manager'
-    : undefined;   // 👈 IMPORTANT
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
